ed9aac8863763a9687699dc86cc15f6681999dc6
arthur <arthur.rios007@gmail.com>
2023-11-17 09:16:37 -0300

    add configs, colors, radii and spacing

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import { Card } from 'ui'
+
+export default function Home() {
+  return (
+    <div>
+      <Card title="Hello World" href="/">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+        deserunt voluptatibus dolor, debitis alias asperiores reiciendis
+        nesciunt dignissimos inventore ipsam totam iste cumque at deleniti cum?
+        Voluptatum officia velit minus.
+      </Card>
+    </div>
+  )
+}
